3c52df50db0e5037291c5b64aececd0126591140
TheNotOnly <darren.ramen@gmail.com>
2015-08-04 20:13:19 +0400

    zram: revalidate disk after capacity change

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -589,8 +589,10 @@ static void zram_reset_device(struct zram *zram, bool reset_capacity)
 	memset(&zram->stats, 0, sizeof(zram->stats));
 
 	zram->disksize = 0;
-	if (reset_capacity)
+	if (reset_capacity) {
 		set_capacity(zram->disk, 0);
+		revalidate_disk(zram->disk);
+	}
 	up_write(&zram->init_lock);
 }
 
@@ -644,6 +646,7 @@ static ssize_t disksize_store(struct device *dev,
 
 	zram->disksize = disksize;
 	set_capacity(zram->disk, zram->disksize >> SECTOR_SHIFT);
+	revalidate_disk(zram->disk);
 	zram_init_device(zram, meta);
 	up_write(&zram->init_lock);
 
