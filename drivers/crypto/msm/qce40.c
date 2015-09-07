92f0cb5fc1111b930149fb4c93816b6a3e38c224
TheNotOnly <darren.ramen@gmail.com>
2015-09-07 22:53:43 +0400

    qce40: Update clock vote for improved performance

--- drivers/crypto/msm/qce40.c
+++ drivers/crypto/msm/qce40.c
@@ -2560,8 +2560,8 @@ void *qce_open(struct platform_device *pdev, int *rc)
 	if (!IS_ERR(ce_core_src_clk)) {
 		pce_dev->ce_core_src_clk = ce_core_src_clk;
 
-		/* Set the core src clk @100Mhz */
-		ret = clk_set_rate(pce_dev->ce_core_src_clk, 100000000);
+		/* Set the core src clk @171Mhz */
+		ret = clk_set_rate(pce_dev->ce_core_src_clk, 171430000);
 		if (ret) {
 			clk_put(pce_dev->ce_core_src_clk);
 			goto err;
